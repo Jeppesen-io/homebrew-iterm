a038dd3594702f8fa0ab9e78ee970a6b544b5fe7
Nelson Jeppesen <nelson@simplymeasured.com>
2016-05-12 21:18:20 -0700

    I keep trying :/

--- Casks/iterm.rb
+++ Casks/iterm.rb
@@ -5,19 +5,19 @@ cask 'iterm' do
     `defaults delete com.googlecode.iterm2.plist`
   end
 
-  version '0.3'
+  version '0.4'
   url 'https://github.com/Jeppesen-io/homebrew-iterm/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-iterm'
   sha256 :no_check
 
   require 'fileutils'
   FileUtils::mkdir_p ENV['HOME'] + '/.local/iterm'
-  FileUtils::mkdir_p ENV['HOME'] + '/.l'
 
-  artifact 'homebrew-iterm-master/dot-files/com.googlecode.iterm2.plist', target: ENV['HOME'] + "/Library/Preferences/com.googlecode.iterm2.plist"
 
   depends_on cask: 'caskroom/homebrew-versions/iterm2-beta'
   depends_on cask: 'caskroom/homebrew-fonts/font-sauce-code-powerline'
+
+  artifact 'homebrew-iterm-master/dot-files/com.googlecode.iterm2.plist', target: ENV['HOME'] + "/Library/Preferences/com.googlecode.iterm2.plist2"
 
   postflight do
 
