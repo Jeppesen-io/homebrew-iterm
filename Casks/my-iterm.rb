0d24f6a3d7fd131249a1ab362aa7f9d45e03a117
Nelson Jeppesen <nelson@simplymeasured.com>
2016-07-11 15:46:23 -0700

    Update iterm setttings - enable auto update

--- Casks/my-iterm.rb
+++ Casks/my-iterm.rb
@@ -5,7 +5,7 @@ cask 'my-iterm' do
     `defaults delete com.googlecode.iterm2.plist`
   end
 
-  version '1.1'
+  version '1.1.1'
   url 'https://github.com/Jeppesen-io/homebrew-iterm/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-iterm'
   sha256 :no_check
