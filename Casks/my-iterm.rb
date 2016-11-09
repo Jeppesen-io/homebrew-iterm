002331b1084b35088ff19e204bf63e0816060277
Nelson Jeppesen <Nelson@SimplyMeasured.com>
2016-11-09 14:35:57 -0800

    Save iterm settings on quit

--- Casks/my-iterm.rb
+++ Casks/my-iterm.rb
@@ -5,24 +5,27 @@ cask 'my-iterm' do
     `defaults delete com.googlecode.iterm2`
   end
 
-  version '1.4'
+  version '1.5'
   url 'https://github.com/Jeppesen-io/homebrew-iterm/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-iterm'
   sha256 :no_check
 
   require 'fileutils'
-  FileUtils::mkdir_p ENV['HOME'] + '/.local/logs'
+  FileUtils::mkdir_p ENV['HOME'] + '/.local/iterm'
+  FileUtils::mkdir_p ENV['HOME'] + '/.config/iterm'
 
 
   depends_on cask: 'iterm2'
   depends_on cask: 'caskroom/fonts/font-source-code-pro-for-powerline'
 
-  artifact 'homebrew-iterm-master/dot-files/com.googlecode.iterm2.plist', target: ENV['HOME'] + "/Library/Preferences/com.googlecode.iterm2.plist.custom"
+  artifact 'homebrew-iterm-master/dot-files/com.googlecode.iterm2.plist', target: ENV['HOME'] + '/.config/iterm/com.googlecode.iterm2.plist'
 
   postflight do
 
     # Load it up!
-    `defaults import com.googlecode.iterm2 #{ENV['HOME']}/Library/Preferences/com.googlecode.iterm2.plist.custom`
+    `delete com.googlecode.iterm2`
+    `defaults write com.googlecode.iterm2 PrefsCustomFolder -string ~/.config/iterm`
+    `defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1`
 
   end
 
