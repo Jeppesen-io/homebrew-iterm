0e01f6e69e702516eb7b0af678331b00f4097d48
Nelson Jeppesen <Nelson@SimplyMeasured.com>
2016-11-09 14:43:11 -0800

    Minor iterm2 updates

--- Casks/my-iterm.rb
+++ Casks/my-iterm.rb
@@ -5,7 +5,7 @@ cask 'my-iterm' do
     `defaults delete com.googlecode.iterm2`
   end
 
-  version '1.5.1'
+  version '1.5.2'
   url 'https://github.com/Jeppesen-io/homebrew-iterm/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-iterm'
   sha256 :no_check
