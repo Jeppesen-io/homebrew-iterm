592ac303937d06c9d5be34e6bd8557acceed3baf
Nelson Jeppesen <Nelson>
2017-09-05 15:51:33 -0700

    Adjust iterm setttings

--- Casks/my-iterm.rb
+++ Casks/my-iterm.rb
@@ -5,7 +5,7 @@ cask 'my-iterm' do
     `defaults delete com.googlecode.iterm2`
   end
 
-  version '1.7.0'
+  version '1.7.1'
   url 'https://github.com/Jeppesen-io/homebrew-iterm/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-iterm'
   sha256 :no_check
@@ -13,7 +13,6 @@ cask 'my-iterm' do
   require 'fileutils'
   FileUtils::mkdir_p ENV['HOME'] + '/.local/iterm'
   FileUtils::mkdir_p ENV['HOME'] + '/.config/iterm'
-
 
   depends_on cask: 'iterm2'
   depends_on cask: 'caskroom/fonts/font-awesome-terminal-fonts'
@@ -28,8 +27,6 @@ cask 'my-iterm' do
     `defaults write com.googlecode.iterm2 PrefsCustomFolder -string ~/.config/iterm`
     `defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1`
     `defaults write com.googlecode.iterm2 moveToApplicationsFolderAlertSuppress -int 1`
-    `defaults write com.googlecode.iterm2 SUAutomaticallyUpdate -int 1`
-    `defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -int 1`
   end
 
 end
